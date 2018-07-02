58af8eef24b3d1bc018a5de115e4bdff6897d478
Learzhu <694125196@qq.com>
2018-07-03 00:20:09 +0800

    Initial commit

---
+++ app/src/main/java/com/learzhu/learzhukotlin/Test.kt
@@ -0,0 +1,21 @@
+package com.learzhu.learzhukotlin
+
+import java.sql.DriverManager.println
+
+/**
+ * ${className}.java是极搜浏览器的$DES$类。
+ *
+ * @author Learzhu
+ * @version 3.0.0 2018/7/3 0:16
+ * @update Learzhu 2018/7/3 0:16
+ * @updateDes
+ * @include {@link }
+ * @used {@link }
+ */
+public class Test {
+    fun main(args: Array<String>) {
+//        println("AAA")
+        println("AA")
+    }
+
+}
