e512d1de311f799284b8189c777fe93508c38127
Learzhu <694125196@qq.com>
2018-07-20 18:20:58 +0800

    1.java Enum

--- app/src/main/java/com/learzhu/learzhukotlin/base/JavaTest.java
+++ app/src/main/java/com/learzhu/learzhukotlin/base/JavaTest.java
@@ -13,5 +13,40 @@ package com.learzhu.learzhukotlin.base;
 public class JavaTest {
     public static void main(String args[]) {
     }
+
 }
+
+enum Color {
+    RED("红色", 1), GREEN("绿色", 2), BLANK("白色", 3), YELLO("黄色", 4);
+    // 成员变量
+    private String name;
+    private int index;
+
+    // 构造方法
+    private Color(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+
+    //覆盖方法
+    @Override
+    public String toString() {
+        return this.index + "_" + this.name;
+    }
+
+    public String switchX(Color color) {
+        switch (color) {
+            case RED:
+                return "AA";
+            case BLANK:
+                return "BB";
+            default:
+                return "CC";
+        }
+    }
+}
+//enum Color(int r,int g,int b){
+//        RED(255,0,0),ORANGE(255,165,0),BLUE(0,0,255);
+////        fun rgb()=(r*256+g)*256+b
+//        }
 
