acc88af441c0a5c5c6e2e2ce8d98e4b48e8645db
Learzhu <694125196@qq.com>
2018-07-20 15:38:06 +0800

    1.JavaTest

---
+++ app/src/main/java/com/learzhu/learzhukotlin/base/JavaTest.java
@@ -0,0 +1,17 @@
+package com.learzhu.learzhukotlin.base;
+
+/**
+ * JavaTest.java是液总汇的类。
+ *
+ * @author Learzhu
+ * @version 2.0.0 2018-07-20 09:19
+ * @update Learzhu 2018-07-20 09:19
+ * @updateDes
+ * @include {@link }
+ * @used {@link }
+ */
+public class JavaTest {
+    public static void main(String args[]) {
+    }
+}
+
