40cad8d1908eb6322dc7a8f3984a7bca95a51a2e
Learzhu <694125196@qq.com>
2018-08-13 14:45:36 +0800

    1.companion 关键字 2.修改代码规范

--- app/src/main/java/com/learzhu/learzhukotlin/class_object_interface/Object.kt
+++ app/src/main/java/com/learzhu/learzhukotlin/class_object_interface/Object.kt
@@ -24,11 +24,27 @@ object Payroll {
 //对象声明可以继承自类和接口
 //object CaseInsensitiveFileComparator : Comparator<File> {
 //    override fun compare(file1: File?, file2: File?): Int {
-//        return file1?.getPath()?.compareTo(file2?.getPath(), ignoreCase = true)
+//        return file1.getPath().compareTo(file2.getPath(),ignoreCase = true)
 //    }
+////    override fun compare(file1: File?, file2: File?): Int {
+////        return file1?.getPath()?.compareTo(file2?.getPath(), ignoreCase = true)
+////    }
 //}
 
 fun main(args: Array<String>) {
     Payroll.allEmployees.add(Person("Alice"))
     Payroll.calculateSalary()
 }
+
+//data object
+//data class Person1(val name: String) {
+//    object NameComparator : Comparator<Person1> {
+//        override fun compare(p1: Person1?, p2: Person1?): Int {
+//            if (p1 != null) {
+//                if (p2 != null) {
+//                    p1.name.compareTo(p2.name)
+//                }
+//            }
+//        }
+//    }
+//}
